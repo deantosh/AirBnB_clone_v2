b2a89970d8455eae8aa519c65d588ffdba398a1e
deantosh <deantoshdaiddoh@gmail.com>
2024-01-20 03:39:06 -0800

    Add a condition to chose between db storage and file storage depending on the value of ENV variable

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 """
-creates a unique FileStorage instance for your application
+0;276;0ccreates a unique FileStorage instance for your application
 """
 
-from models.engine.file_storage import FileStorage
+from os import environ
 
+if environ.get('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 
-storage = FileStorage()
 storage.reload()
