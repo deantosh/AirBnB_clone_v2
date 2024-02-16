7c48dcf7016ad58545987a9022cc1aa6b24f17a1
root <deantoshdaiddoh@gmail.com>
2024-02-16 05:03:47 -0800

    Task 2: solution added

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Script starts a Flask web application.
+web application must be listening on 0.0.0.0, port 5000.
+
+Routes:
+   /: display “Hello HBNB!”
+   /hbnb: display “HBNB”
+   /c/<text>: display “C ” followed by the value of the text
+              variable (replace underscore _ symbols with a space )
+
+Use the option strict_slashes=False
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def display_some_text(text, strict_slashes=False):
+    # process text replace _ with " ".
+    new_text = text.replace("_", " ")
+    result = "C " + new_text
+    return result
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
