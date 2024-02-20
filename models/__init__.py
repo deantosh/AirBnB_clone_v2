6fc53a8b3103c99e8d26b336f543244de5a7b1d4
root <deantoshdaiddoh@gmail.com>
2024-02-19 23:34:36 -0800

    File: updated

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """
-0;276;0ccreates a unique FileStorage instance for your application
+Creates a unique FileStorage instance for your application
 """
 
 from os import environ
 
-if environ.get('HBNB_TYPE_STORAGE') == 'db':
+# get storage type from ENVIRON
+storage_type = environ.get("HBNB_TYPE_STORAGE")
+
+if storage_type == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
