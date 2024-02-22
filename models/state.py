9ac43be3db218ad673152c49af4a1bf5f3879cf1
root <deantoshdaiddoh@gmail.com>
2024-02-22 10:39:11 -0800

    Task 7: solution added

--- models/state.py
+++ models/state.py
@@ -9,16 +9,17 @@ import models
 
 
 class State(BaseModel, Base):
-    """creates the state record -- DB Storage"""
+    """creates the state record --  DbStorage"""
     __tablename__ = 'states'
 
     name = Column(String(128), nullable=False)
     cities = relationship(
         'City', backref='state', cascade="all, delete-orphan")
 
-    # create a state object -- FileStorage
+    # FileStorage
+
     def __init__(self, name, *args, **kwargs):
-        """initialize the state class"""
+        """initialize state"""
         super().__init__(args, kwargs)
         self.name = name
 
@@ -30,7 +31,9 @@ class State(BaseModel, Base):
            State and City.
         """
         city_list = []
-        for city_obj in models.storage.all(City).values():
+
+        cities = models.storage.all(City).values()
+        for city_obj in cities:
             if city_obj.state_id == self.id:
                 city_list.append(city_obj)
         return city_list
